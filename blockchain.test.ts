ddf69c6e4c64fa1892a11af0ca2666c76626a262
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-03-21 15:06:46 -0400

    Add blockchain test

--- blockchain.test.ts
+++ blockchain.test.ts
@@ -18,4 +18,24 @@ describe("Blockchain", () => {
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
   });
+
+  describe("isValidChain()", () => {
+    describe("when the chain does not start with the genesis block", () => {
+      test("returns false", () => {
+        blockchain.chain[0] = { data: ["fake-genesis"] } as Block;
+        expect(BlockChain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+    describe("when the chain starts with the genesis block and has multiple blocks", () => {
+      describe("and a lastHash reference has changed", () => {
+        test("returns false", () => {});
+      });
+      describe("and the chain contains a block with an invalid field", () => {
+        test("returns false", () => {});
+      });
+      describe("and the chain does not contain any invalid blocks", () => {
+        test("returns true", () => {});
+      });
+    });
+  });
 });
